f92bec5b08743c5e330b1c2a7622c10387741294
bendohou <houssemebendou@gmail.com>
2024-12-18 20:34:50 +0100

    application

--- client/src/App.js
+++ client/src/App.js
@@ -31,7 +31,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React youva
         </a>
       </header>
     </div>
